922a89c5b526460cc0d4cf24f33f3d98f60b4e20
ivan-ivano <ivan.ivano.ri.2023@lpnu.ua>
2023-10-31 19:10:58 +0200

    2

--- test_LAB6.3_rek/test_LAB6.3_rek.cpp
+++ test_LAB6.3_rek/test_LAB6.3_rek.cpp
@@ -1,5 +1,6 @@
 #include "pch.h"
-#include "CppUnitTest.h"
+#include "CppUnitTest.h".
+#include "../LAB6.3_rek/LAB6.3_rek.cpp"
 
 using namespace Microsoft::VisualStudio::CppUnitTestFramework;
 
@@ -11,7 +12,9 @@ namespace testLAB63rek
 		
 		TEST_METHOD(TestMethod1)
 		{
-
+			int a[5] = { 2, 5, 1, 9, 2 };
+			int t = arr_sum(a, 5);
+			Assert::AreEqual(t, 19);
 		}
 	};
 }
